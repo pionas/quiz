02d95d7f039372528356641845e71e2ed9f6fee3
Adrian Pionka <adrian@pionka.com>
2023-11-02 20:06:16 +0100

    add static imports

--- src/test/java/info/pionas/quiz/api/quiz/QuizRestControllerIT.java
+++ src/test/java/info/pionas/quiz/api/quiz/QuizRestControllerIT.java
@@ -2,7 +2,6 @@ package info.pionas.quiz.api.quiz;
 
 import info.pionas.quiz.api.AbstractIT;
 import info.pionas.quiz.infrastructure.database.quiz.QuizEntity;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
@@ -10,6 +9,9 @@ import org.springframework.web.client.HttpClientErrorException;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 
 class QuizRestControllerIT extends AbstractIT {
 
@@ -20,18 +22,18 @@ class QuizRestControllerIT extends AbstractIT {
         //when
         final var response = apiRestClient.postForEntity("/quiz", newQuizDto, QuizDto.class);
         //then
-        Assertions.assertThat(response).isNotNull();
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         QuizDto quizDto = response.getBody();
-        Assertions.assertThat(quizDto).isNotNull();
-        Assertions.assertThat(quizDto.getTitle()).isEqualTo(newQuizDto.getTitle());
-        Assertions.assertThat(quizDto.getDescription()).isEqualTo(newQuizDto.getDescription());
+        assertThat(quizDto).isNotNull();
+        assertThat(quizDto.getTitle()).isEqualTo(newQuizDto.getTitle());
+        assertThat(quizDto.getDescription()).isEqualTo(newQuizDto.getDescription());
         List<QuestionDto> questions = quizDto.getQuestions();
-        Assertions.assertThat(questions).hasSize(2);
+        assertThat(questions).hasSize(2);
         final var quizEntity = dbUtil.em().find(QuizEntity.class, quizDto.getId());
-        Assertions.assertThat(quizEntity).isNotNull();
-        Assertions.assertThat(quizEntity.getTitle()).isEqualTo(newQuizDto.getTitle());
-        Assertions.assertThat(quizEntity.getDescription()).isEqualTo(newQuizDto.getDescription());
+        assertThat(quizEntity).isNotNull();
+        assertThat(quizEntity.getTitle()).isEqualTo(newQuizDto.getTitle());
+        assertThat(quizEntity.getDescription()).isEqualTo(newQuizDto.getDescription());
 
     }
 
@@ -40,16 +42,16 @@ class QuizRestControllerIT extends AbstractIT {
         //given
         final var newQuizDto = NewQuizDto.builder().build();
         //when
-        final var exception = Assertions.catchThrowableOfType(() -> apiRestClient.postForEntity("/quiz", newQuizDto, QuizDto.class), HttpClientErrorException.class);
+        final var exception = catchThrowableOfType(() -> apiRestClient.postForEntity("/quiz", newQuizDto, QuizDto.class), HttpClientErrorException.class);
         //then
-        Assertions.assertThat(exception).isNotNull();
-        Assertions.assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-        Assertions.assertThat(exception.getResponseBodyAsByteArray()).isNotNull();
+        assertThat(exception).isNotNull();
+        assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(exception.getResponseBodyAsByteArray()).isNotNull();
         final var errorJson = objectMapper.readTree(exception.getResponseBodyAsByteArray());
         final var errors = StreamSupport
                 .stream(errorJson.get("errors").spliterator(), false)
                 .toList();
-        Assertions.assertThat(errors.get(0).asText()).isEqualTo("title: must not be blank");
+        assertThat(errors.get(0).asText()).isEqualTo("title: must not be blank");
     }
 
     private NewQuizDto getNewQuizDto() {
